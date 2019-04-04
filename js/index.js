cf865b7399446e7f2be86f3fa5f71544b36dd697
tchang9 <tlchang@gmail.com>
2019-04-04 09:15:38 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,33 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
+
+document.addEventListener("DOMContentLoaded", function(){
+  const saveLincoln = document.querySelector("#save_lincoln")
+  const hiddenMessage = document.querySelector("#hidden-message")
+
+  saveLincoln.addEventListener("click", function(e) {
+    fadeOutEffect()
+    hiddenMessage.style.display = ""
+  })
+})
+
+function fadeOutEffect() {
+  var fadeTarget = document.querySelector("#foreground")
+  const userInputTag = document.querySelector("#interval")
+  var fadeEffect = setInterval(function () {
+      if (!fadeTarget.style.opacity) {
+          fadeTarget.style.opacity = 1;
+      }
+      if (fadeTarget.style.opacity > 0) {
+          fadeTarget.style.opacity -= 0.01;
+      } else {
+          clearInterval(fadeEffect);
+      }
+  }, userInputTag.value);
 }
